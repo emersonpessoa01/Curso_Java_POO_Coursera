08fe7a08c518ceb2438eff511e2c91639f25b8d8
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-07-16 17:38:33 -0300

    :rocket:

--- src/coursera/módulo1/Carros/src/test/java/org/coursera/Carros/TesteCarro.java
+++ src/coursera/módulo1/Carros/src/test/java/org/coursera/Carros/TesteCarro.java
@@ -7,29 +7,25 @@ import org.junit.jupiter.api.Test;
 
 class TesteCarro {
 	Carro c;
-	
+
 	@Before
 	public void inicializaCarro() {
-		c = new Carro("teste",10,100);
+		c = new Carro("teste", 10, 100);
+	}
+
+	@Test
+	public void testCarroParado() {
+		inicializaCarro();
+		assertEquals(0, c.getVelocidade());
 	}
 
 	@Test
 	public void testAcelerar() {
 		inicializaCarro();
 		c.acelerar();
-		assertEquals(10,c.getVelocidade());
+		assertEquals(10, c.getVelocidade());
 	}
-	@Test
-	public void testCarroParado() {
-		inicializaCarro();
-		assertEquals(0,c.getVelocidade());
-	}
-	public void testFrear() {
-		inicializaCarro();
-		c.acelerar();
-		c.frear();
-		assertEquals(5,c.getVelocidade());
-	}
+
 	public void testFrearAteZero() {
 		inicializaCarro();
 		c.acelerar();
@@ -37,15 +33,22 @@ class TesteCarro {
 		c.frear();
 		c.frear();
 		c.frear();
-		assertEquals(0,c.getVelocidade());
+		assertEquals(0, c.getVelocidade());
 	}
+
+	public void testFrear() {
+		inicializaCarro();
+		c.acelerar();
+		c.frear();
+		assertEquals(5, c.getVelocidade());
+	}
+
 	public void testAcelerarAteVelocidadeMaxima() {
-		for(int i = 0;i <14;i++) {
+		for (int i = 0; i < 14; i++) {
 			c.acelerar();
-			assertEquals(100,c.getVelocidade());	
+			assertEquals(100, c.getVelocidade());
 		}
-		
-		
+
 	}
 
 }
